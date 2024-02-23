d2f0a6b82e97fa3522e90e5e4accd4f56f528cc1
aleriiu <aleriiiu@gmail.com>
2024-02-23 13:32:52 +0300

    added modal menu

--- script.js
+++ script.js
@@ -57,3 +57,17 @@ slider.addEventListener("input", function() {
       break;
   }
 });
+
+// modal window 
+
+const closeBtn = document.querySelector('#close');
+const modalMenu = document.querySelector('#modal_menu__wrapper');
+const mobMenu = document.querySelector('#mob-menu');
+
+mobMenu.addEventListener('click', () => {
+    modalMenu.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    modalMenu.style.display = 'none';
+})
